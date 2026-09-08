f9f2aaa0c1fcdbc8122dd2784513483064b6a095
foisbraian <foisbraian.br@gmail.com>
2026-09-08 16:49:42 -0300

    fix calendar2

--- backend/alter_db.py
+++ backend/alter_db.py
@@ -5,6 +5,8 @@ def upgrade_db():
     migraciones = [
         "ALTER TABLE grupos ADD COLUMN es_mix_grupo BOOLEAN DEFAULT FALSE",
         "ALTER TABLE fechas_evento ADD COLUMN es_mix_evento BOOLEAN DEFAULT FALSE",
+        "ALTER TABLE grupos ADD COLUMN multiparque_acceso BOOLEAN DEFAULT FALSE",
+        "ALTER TABLE grupos ADD COLUMN parador_acceso BOOLEAN DEFAULT FALSE",
     ]
     with engine.begin() as conn:
         for sql in migraciones:
